099a109546c2aa7c1bdfdb92c27bdf954350b8d8
Steven Jurado Mendoza <sjurado@intuitosa.com>
2021-12-02 01:09:19 -0500

    Se agrego portal web

---
+++ Middleware/AccountMiddleware.cs
@@ -0,0 +1,43 @@
+﻿namespace ups.delivey.portal.Middleware
+{
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Http;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    public class AccountMiddleware
+    {
+        private readonly RequestDelegate _requestDelegate;
+
+        public AccountMiddleware(RequestDelegate requestDelegate)
+        {
+            _requestDelegate = requestDelegate;
+        }
+
+        public Task Invoke(HttpContext httpContext)
+        {
+            var path = httpContext.Request.Path;
+
+            if (path.HasValue && path.Value.StartsWith("/Home/Index"))
+            {
+                if (httpContext.Session.GetString("Manager") == null)
+                {
+                    httpContext.Response.Redirect("/");
+                }
+            }
+
+            return _requestDelegate(httpContext);
+        }
+            
+    }
+
+    public static class AccounMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseAccountMilddware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<AccountMiddleware>();
+        }
+    }
+}
---
+++ Pages/Account/Index.cshtml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ups.delivey.portal.Pages
+{
+    public class IndexModel : PageModel
+    {
+        [TempData]
+        public string Message { get; set; }
+
+        public void OnGet()
+        {
+            HttpContext.Session.Remove("Manager");
+        }
+
+        public IActionResult OnPost() 
+        {
+            if (Request.Form["UserName"].Equals("admin") && Request.Form["Password"].Equals("123"))
+            {
+                HttpContext.Session.SetString("Manager","True");
+                return Redirect("/Home/Index");
+            }
+            else
+            {
+                Message = "Wrong, Please verify your email or password";
+                return Page();
+            }
+        }
+    }
+}
